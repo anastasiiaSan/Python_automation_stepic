cf8829a60e7cf5c456818a31e3ee8b344f41418d
anastasiiaSan <60625623+anastasiiaSan@users.noreply.github.com>
2020-02-16 15:36:13 +0100

    Update constest.py
    Default language "English" was set

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ import time
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--language', action='store', default=None,
+    parser.addoption('--language', action='store', default="en",
                      help="Choose locale language")
 
 
