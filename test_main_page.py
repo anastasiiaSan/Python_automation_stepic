4adaf0d02569e21bac75c330defbb756739e0a02
anastasiya <anastasiya.sandalova@gmail.com>
2020-02-16 17:30:40 +0100

    page objects were added, test main page was modified

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,10 @@
 import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
     time.sleep(10)
-    login_link.click()
